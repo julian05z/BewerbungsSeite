fe66e26d92ed06f5cbefa5871d40c88e442897a3
J.Zurkinden.inf21 <j.zurkinden.inf21@stud.bbbaden.ch>
2023-07-31 14:58:31 +0200

    Weitere Anpassungen getroffen und abgeschlossen

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  const dynamicText = document.querySelector(".title span");
+  const words = ["Julian Zurkinden"];
+  
+  let wordIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  
+  const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = isDeleting
+      ? currentWord.substring(0, charIndex - 1)
+      : currentWord.substring(0, charIndex + 1);
+  
+    dynamicText.textContent = currentChar;
+  
+    if (!isDeleting && charIndex <= currentWord.length) {
+      charIndex++;
+      setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex >= 0) {
+      charIndex--;
+      setTimeout(typeEffect, 100);
+    } else {
+      isDeleting = !isDeleting;
+      wordIndex = (wordIndex + 1) % words.length;
+      setTimeout(typeEffect, 1200);
+    }
+  };
+  
+  typeEffect();
